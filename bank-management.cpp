663b034d97b6daf6d1eb922ef11fca3e3b726de7
Talib <talibdolovac15@gmail.com>
2023-01-22 08:19:54 +0100

    Bank Management CPP

--- bank-management.cpp
+++ bank-management.cpp
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <regex>
 #include <stdlib.h>
+#include <cstdio>
 using namespace std;
 
 // Structure to store user information
@@ -21,7 +22,10 @@ void login();
 bool checkCredentials(string email, string password);
 bool validateEmail(string email);
 void withdrawMoney(string email, string password);
-void transactionMoney();
+void transactionMoney(string email, string password);
+void checkBalance(string email, string password);
+void displayAccounts();
+void closeAccount(string email, string password);
 
 int main() {
   cout << "Welcome to the bank management system." << endl;
@@ -120,8 +124,11 @@ void login() {
 	cout << "Successful login." << endl;
 	
 	cout << "1. Withdraw Money." << endl;
-  cout << "2. Transaction Money" << endl;
-	cout << "3. Exit." << endl;
+  cout << "2. Transaction Money." << endl;
+  cout << "3. Check Balance." << endl;
+  cout << "4. Display Accounts." << endl;
+  cout << "5. Close Account." << endl;
+	cout << "6. Exit." << endl;
 	cout << "?:" << endl;
 	cin >> izbor;
 	
@@ -130,12 +137,24 @@ void login() {
 			withdrawMoney(email, password);
 			break;
     case 2: 
-      transactionMoney();
+      transactionMoney(email, password);
       break;
-		case 3:
-			exit(2);
+    case 3:
+			checkBalance(email, password);
 			break;
-	}
+		case 4:
+			displayAccounts();
+			break;
+		case 5:
+			closeAccount(email, password);
+			break;
+		case 6:
+			exit(6);
+			break;
+		default:
+      cout << "Invalid choice." << endl;
+      break;
+	} 
   } else {
     cout << "Unsuccessful login." << endl;
   }
@@ -251,36 +270,189 @@ void withdrawMoney(string email, string password) {
   cout << "Error: User not found." << endl;
 }
 
-void transactionMoney() {
-  int transcMoney;
-  string username;
-  bool isUsername = false;
-  
+void transactionMoney(string email, string password) {
+  // Open the file to read user data
   ifstream infile;
   infile.open("users.txt");
 
   string line;
-  
+  string name, email_from_file, password_from_file;
+  int balance;
+  bool found = false;
+
+  // Read the file line by line
   while (getline(infile, line)) {
-  
-		string fields[4];  
-    int i = 0;
-    string field;
+    // Use a stringstream to extract the data from the line
     stringstream ss(line);
-    while (getline(ss, field, ',')) {
-      fields[i] = field;
-      i++;
+
+    // Use getline() to extract the data from the stringstream
+    getline(ss, name, ',');
+    getline(ss, email_from_file, ',');
+    getline(ss, password_from_file, ',');
+    ss >> balance;
+
+    if (email == email_from_file && password == password_from_file) {
+      // User is found
+      found = true;
+      break;
     }
-    
-    if(fields[0] == username) {
-    	isUsername = true;
-		}
   }
-		cout << "Enter the username of the person: ";
-	  cin >> username;
-	
-	  if(isUsername) {
-	    cout << "How much $:";
-			cin >> transcMoney;
-	  }
+
+  // Close the file
+  infile.close();
+
+  if (found) {
+    // User is found, proceed with transaction
+    int amount;
+    string email_to;
+
+    cout << "Enter the email of the user you want to transaction to: ";
+    cin >> email_to;
+
+    cout << "Enter the amount you want to transaction: ";
+    cin >> amount;
+
+    if (amount > balance) {
+      cout << "Error: Insufficient funds." << endl;
+    } else {
+      // Open the file to update user data
+      fstream file;
+      file.open("users.txt", ios::in);
+
+      stringstream ss;
+      string line_from_file;
+
+      while (getline(file, line_from_file)) {
+        stringstream line_ss(line_from_file);
+
+        getline(line_ss, name, ',');
+        getline(line_ss, email_from_file, ',');
+        getline(line_ss, password_from_file, ',');
+        line_ss >> balance;
+
+        if (email == email_from_file && password == password_from_file) {
+          // Update the balance of the current user
+          balance -= amount;
+        }
+
+        if (email_to == email_from_file) {
+          // Update the balance of the user to transaction to
+          balance += amount;
+        }
+
+        // Write the updated data to the stringstream
+        ss << name << "," << email_from_file << "," << password_from_file << "," << balance << endl;
+      }
+
+      // Close the file
+      file.close();
+
+      // Open the file to write the updated data
+      ofstream outfile;
+      outfile.open("users.txt");
+
+      // Write the data from the stringstream to the file
+      outfile << ss.str();
+
+      // Close the file
+      outfile.close();
+
+      cout << "Transaction successful." << endl;
+    }
+  } else {
+    // User is not found
+    cout << "Error: User not found." << endl;
+  }
 }
+
+void checkBalance(string email, string password) {
+  // Open the file in read mode
+  ifstream infile;
+  infile.open("users.txt");
+
+  // Read the file line by line
+  string line;
+  while (getline(infile, line)) {
+    string name, email_, password_, balance;
+    stringstream ss(line);
+    getline(ss, name, ',');
+    getline(ss, email_, ',');
+    getline(ss, password_, ',');
+    getline(ss, balance, ',');
+
+    // Check if the email and password match
+    if (email_ == email && password_ == password) {
+      // Print the balance
+      cout << "Your balance is: $" << balance << endl;
+      return;
+    }
+  }
+
+  // Close the file
+  infile.close();
+
+  cout << "Invalid email or password." << endl;
+}
+
+void displayAccounts() {
+    ifstream infile;
+    infile.open("users.txt");
+
+    cout << "Username" << "\t" << "Email" << "\t" << "Balance" << endl;
+    cout << "-------------------------------------------------" << endl;
+
+    string line;
+    while (getline(infile, line)) {
+        stringstream ss(line);
+        string name, email, password, balance;
+        getline(ss, name, ',');
+        getline(ss, email, ',');
+        getline(ss, password, ',');
+        getline(ss, balance);
+
+        cout << name << "\t" << email << "\t" << balance << endl;
+    }
+    infile.close();
+}
+
+void closeAccount(string email, string password) {
+    // check if the provided email and password match the ones on file
+    if (!checkCredentials(email, password)) {
+        cout << "Error: Invalid email or password." << endl;
+        return;
+    }
+
+    // temp file to store the valid users
+    ofstream temp;
+    temp.open("temp.txt");
+
+    // read the "users.txt" file line by line
+    ifstream infile;
+    infile.open("users.txt");
+    string line;
+    while (getline(infile, line)) {
+        stringstream ss(line);
+        string name, email_, password_, balance;
+        getline(ss, name, ',');
+        getline(ss, email_, ',');
+        getline(ss, password_, ',');
+        getline(ss, balance);
+
+        // if the email is not the one being deleted, write to the temp file
+        if (email != email_) {
+            temp << line << endl;
+        }
+    }
+    infile.close();
+    temp.close();
+
+    // remove the original "users.txt" file
+    remove("users.txt");
+
+    // rename the temp file to "users.txt"
+    rename("temp.txt", "users.txt");
+    cout << "Successfully closed account" << endl;
+    //log out
+    exit(3);
+}
+
